c9bcabfec18282fdc37de15e9261f29125e183a2
Rashad DuPaty <radupaty@gmail.com>
2024-01-06 14:16:04 +0900

    Added 'Show' route, controller/action, view

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,12 @@ Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index', as: 'tasks'
 
   # Create
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  post 'tasks', to: 'tasks#create'
 
-  # Read
+  # Read One
+  # The "show" action refers to the single target from the full index
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
 
   # Update
 
